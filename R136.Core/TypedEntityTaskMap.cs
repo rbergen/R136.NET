c4bad3d7a69448751be5788f855ed654dd476a31
Rutger van Bergen <rbergen@xs4all.nl>
2021-02-26 08:37:28 +0100

    Update TypedEntityTaskMap.cs

--- R136.Core/TypedEntityTaskMap.cs
+++ R136.Core/TypedEntityTaskMap.cs
@@ -8,12 +8,12 @@ namespace R136.Core
 {
 	public class TypedEntityCollection
 	{
-		private Dictionary<Type, object> _map = new Dictionary<Type, object>();
+		private Dictionary<Type, Task> _map = new Dictionary<Type, Task>();
 
-		public void Add<TValue>(TValue value) where TValue : notnull
+		public void Add<TValue>(Task<TValue> value) where TValue : notnull
 			=> _map[typeof(TValue)] = value;
 
 		public TValue Get<TValue>()
-			=> (TValue)_map[typeof(TValue)];
+			=> ((Task<TValue>)_map[typeof(TValue)]).Result;
 	}
 }
